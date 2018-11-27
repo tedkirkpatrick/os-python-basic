21964f614d203c857721945b7e5008c235a58ea9
Ted Kirkpatrick <ted@sfu.ca>
2018-11-26 17:54:23 -0800

    Add mondo booga

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Salut, monde!"
+    return "Salut, mondo Booga!"
 
 if __name__ == "__main__":
     application.run()
