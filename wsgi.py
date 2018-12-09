5aa08f3ae6c9aa7b4b7bcf92e5c97cd912eb0519
Ted Kirkpatrick <ted@sfu.ca>
2018-12-09 08:58:57 -0800

    Append URL of call

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask
+form flask import request
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Coordinated, this time?"
+    return "Coordinated, this time?: " + request.url
 
 if __name__ == "__main__":
     application.run()
